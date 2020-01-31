7a1bddc74a6155c0f2d8a3bbb3bb0afc71a149cd
nalhin <krol.wow@gmail.com>
2020-02-01 00:36:59 +0100

    Added project structure.

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "@vue/cli-plugin-unit-jest",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{vue}",
+    "!**/node_modules/**",
+    "!<rootDir>/dist/**",
+    "!<rootDir>/src/plugins/**",
+    "!<rootDir>/tests/**"
+  ]
+};
